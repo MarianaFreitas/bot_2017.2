dcb936b57a59434ac05f70ee73bcdf3a50820d64
MaryFreitas <37901460+MaryFreitas@users.noreply.github.com>
2018-04-20 08:47:34 -0300

    Update Pessoa.java

--- aula06/src/entity/Pessoa.java
+++ aula06/src/entity/Pessoa.java
@@ -1,58 +1,80 @@
 package entity;
 
-//classes abstratas n„o podem ser instanciadas (n„o funciona new pessoa)
+// classes abstratas n√£o podem ser instanciadas (n√£o funciona new Pessoa)
 public abstract class Pessoa {
-
+	
 	protected Integer id;
 	protected String matricula;
 	protected String nome;
 	protected String email;
 	protected String turno;
 	
+	protected Endereco endereco;
 	
+	// construtor sem argumentos
 	public Pessoa() {
+		
+	}
 	
+	public Pessoa(Integer id, String matricula, String nome, String email, String turno) {
+		this.id = id;
+		this.matricula = matricula;
+		this.nome = nome;
+		this.email = email;
+		this.turno = turno;
 	}
 
+
+	// gets e sets (alt + shift + S, R)
 	public Integer getId() {
 		return id;
 	}
-
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-
 	public String getMatricula() {
 		return matricula;
 	}
 
-
-	public void setMatricula(String i) {
-		this.matricula = i;
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	// sobrecarga (equals e hashcode) - alt + shift + S, H
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -63,7 +85,6 @@ public abstract class Pessoa {
 		return result;
 	}
 
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -73,48 +94,36 @@ public abstract class Pessoa {
 		if (getClass() != obj.getClass())
 			return false;
 		Pessoa other = (Pessoa) obj;
-		if (email != null) {
-			if (other.email != null) 
+		if (email == null) {
+			if (other.email != null)
 				return false;
 		} else if (!email.equals(other.email))
 			return false;
-		if (id != null) {
-			if (other.id != null) 
+		if (id == null) {
+			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (matricula != null) {
+		if (matricula == null) {
 			if (other.matricula != null)
 				return false;
 		} else if (!matricula.equals(other.matricula))
 			return false;
 		return true;
-	}	
-
-	//mÈtodo personalizado
-	public String geraPerfil() {
-		
-		return "Os dados s„o:\n"
-				+ "Matricula: " + matricula
-				+ "\nNome: " + nome
-				+ "\nEmail: " + email;
 	}
-
+	
 	@Override
 	public String toString() {
 		return geraPerfil();
 	}
-
-	public String geraMedia() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getTurno() {
-		return turno;
-	}
-
-	public void setTurno(String turno) {
-		this.turno = turno;
+	
+	// metodo personalizado
+	protected String geraPerfil() {
+		return "Os dados s√£o:\n"
+				+ "Matricula: " + matricula
+				+ "\nNome: " 	+ nome
+				+ "\nEmail: " 	+ email
+				+ "\nTurno: " 	+ turno
+				+ "\nEndere√ßo: " 	+ endereco;
 	}
 }
