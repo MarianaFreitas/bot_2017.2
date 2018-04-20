7df14cbf4c495f355df22b14507178be428d3ffd
MaryFreitas <37901460+MaryFreitas@users.noreply.github.com>
2018-04-20 08:42:55 -0300

    Update TestaAlunoGra.java

--- aula06/src/aplication/TestaAlunoGra.java
+++ aula06/src/aplication/TestaAlunoGra.java
@@ -1,18 +1,30 @@
 package aplication;
 
 import entity.AlunoGra;
+import entity.Endereco;
 
 public class TestaAlunoGra {
-
 	public static void main(String[] args) {
-		
-		AlunoGra alung = new AlunoGra();
 
-		alung.setNota1(6.0);
-		alung.setNota2(7.0);
-		alung.setPeriodo(2);
-		alung.setTurno("Manh�");
+		AlunoGra aluGra = new AlunoGra();
 		
-		System.out.println(alung);
+		aluGra.setNome("Daniel Luna");
+		aluGra.setEmail("dluna@gmail.com");
+		aluGra.setMatricula("agra001");
+		aluGra.setPeriodo(2);
+		aluGra.setTurno("Manhã");
+		aluGra.setNota01(10.);
+		aluGra.setNota02(9.8);
+		
+		// Atribuindo a instancia de endereço ao obj de prof
+		aluGra.setEndereco(new Endereco());
+		
+		aluGra.getEndereco().setId(123);
+		aluGra.getEndereco().setLogradouro("Rua 0");
+		aluGra.getEndereco().setBairro("Centro");
+		aluGra.getEndereco().setCidade("Rio de Janeiro");
+		aluGra.getEndereco().setCep("21000-000");
+		
+		System.out.println(aluGra);
 	}
 }
